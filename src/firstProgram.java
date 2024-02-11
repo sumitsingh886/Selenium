f6470bf15f44c20bae021847967314624ebbf5c6
dell <sumit.singh_cs18@gla.ac.in>
2024-02-11 23:05:21 +0530

    First Program

---
+++ src/firstProgram.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class firstProgram {
+//    psvm
+    public static void main(String args[]){
+        WebDriver driver = new FirefoxDriver();
+//        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.getTitle();
+        System.out.println(driver.getTitle());
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        message.getText();
+        System.out.println(message.getText());
+        driver.quit();
+    }
+}
