6ca0dd3fb5f59f14f302c797666966ee7303b6e4
dell <sumit.singh_cs18@gla.ac.in>
2024-03-11 22:58:51 +0530

    Get Commands

--- src/WebDriverCommand/GetCommand.java
+++ src/WebDriverCommand/GetCommand.java
@@ -3,7 +3,6 @@ package WebDriverCommand;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GetCommand {
