6b1b7e26b09556d897caba7c158fba2c6280b218
dell <sumit.singh_cs18@gla.ac.in>
2024-03-12 22:23:13 +0530

    Conditional Commands

--- src/WebDriverCommand/GetCommand.java
+++ src/WebDriverCommand/GetCommand.java
@@ -4,11 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GetCommand {
     public static void main(String[] args) {
-        WebDriver driver  = new FirefoxDriver();
+        WebDriver driver  = new ChromeDriver();
 
         // Get Commands
 
